96d4f47e7d4f8e9f2335b3733d9ae88aab9ef86d
ting0608 <t.chungcheng@gmail.com>
2024-01-22 09:52:21 +0800

    stack and navigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,11 +1,40 @@
 import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from '../screens/Homescreen';
+import ProfileScreen from '../screens/ProfileScreen/ProfileScreen';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Text, View, Image} from 'react-native';
+import logo from '../assets/images/Logo_pokegram.png';
+
+const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <HomeScreen />
+      <Stack.Navigator
+        initialRouteName="Feed"
+        screenOptions={{headerShown: true}}>
+        <Stack.Screen
+          name="Feed"
+          component={HomeScreen}
+          options={{headerTitle: HeaderTitle, headerTitleAlign: 'center'}}
+        />
+        <Stack.Screen
+          name="UserProfile"
+          component={ProfileScreen}
+          options={{title: 'Profile'}}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
+  );
+};
+
+const HeaderTitle = () => {
+  return (
+    <Image
+      source={logo}
+      resizeMode="contain"
+      style={{width: 150, height: 40, padding: 10}}
+    />
   );
 };
 
